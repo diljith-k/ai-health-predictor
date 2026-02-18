1ec6cd98378318f265d5fa27848655c25bac5773
Diljith K <diljithgithub@gmail.com>
2026-02-18 18:06:23 +0530

    fixed internal server error

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ symptoms_list = sorted(list(set(symptoms_list)))
 def home():
     prediction = None
     confidence = None
-    symptoms = None
+    symptoms = []
 
     if request.method == "POST":
         selected_symptoms = request.form.getlist("symptoms")
