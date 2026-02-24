d90bf7bc36b711bfa24f9fff3375d5d6a258c195
Diljith K <diljithgithub@gmail.com>
2026-02-25 00:28:05 +0530

    fix vercel file paths and flask setup

--- api/index.py
+++ api/index.py
@@ -4,8 +4,8 @@ import os
 
 app = Flask(
     __name__,
-    template_folder="../templates",   # important
-    static_folder="../static"
+    template_folder="templates",   # important
+    static_folder="static"
 )
 
 # -------------------------
@@ -60,4 +60,4 @@ def home():
 # Run locally
 # -------------------------
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run()
